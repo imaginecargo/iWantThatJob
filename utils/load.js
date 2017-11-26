2541d369f5deb5528028ebeb4105efbe0153db99
Martin Eigenmann <github@eigenmannmartin.ch>
2017-11-26 21:38:39 +0100

    refactor(utils): removed unused/duplicated code

--- utils/load.js
+++ utils/load.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import store from './store';
 
-const wait = ms => () => new Promise(resolve => setTimeout(resolve, ms));
-
 const load = file => () =>
     axios
         .get(`http://mature-sense.surge.sh/${file}`, {
