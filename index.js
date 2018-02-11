4a2c881b6980c90794e1c6b66bd22f0922185ed6
ffischer1984 <ffischer1984@googlemail.com>
2018-02-11 23:05:26 +0100

    in case it doesn't work....

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ if(!shell.which("go")){
             shell.echo("this script works only outside of an IDE - please start it on a normal terminal")
         }
     }
-    shell.exec("Something is wrong with your GoLang installation - Have you GoLang:1.9? Have you set and exported a GOBIN enivornment variable? - please look in the readme");
+    shell.echo("Something is wrong with your GoLang installation - Have you GoLang:1.9? Have you set and exported a GOBIN enivornment variable? - please look in the readme");
+    shell.echo("sorry that it doesn't work on your machine. In case you're only interested in the results you can view it here: http://207.154.214.32/geocv/#/welcome. But be patient this machine is cheap and slow ;)");
     shell.exit(1);
 }
 if (!shell.which('git')) {
